7cb7463e8e864d835264e490945da5f14fa42894
walid28-creatif <azzouzi.walid28@gmail.com>
2021-02-01 23:00:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import JSXImage from "./assets/jsx-src.png";
+import JSXImage1 from "./assets/code2.png";
+import JSXImage2 from "./assets/Arrays.mp4";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>bienvenu dans GomyCode</h1>
+        <img src="jsx.png" alt="" style={{ width: "30%" }} />
+
+        <img src={JSXImage} alt="" style={{ width: "30%" }} />
+
+        <img src="code2.png" alt="" style={{ width: "30%" }} />
+
+        <img src={JSXImage1} alt="" style={{ width: "30%" }} />
+      </div>
+      <video width={1080} height={720} controls>
+        <source src={JSXImage2} type="video/mp4" />
+      </video>
     </div>
   );
 }
